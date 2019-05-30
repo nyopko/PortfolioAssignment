75fa8b55f8a495e085ea7817b5a5477e4c01be6e
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-05-30 15:11:35 -0400

    Added moving text, added the link to the index, and added the function and style for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,25 @@ $("#myButton").click(function() {
     }, 2000);
 });
 
+$('.ml6 .letters').each(function(){
+    $(this).html($(this).text().replace(/[-A-Za-z0-9!$#%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/g, "<span class='letter'>$&</span>"));
+  });
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml6  .letter',
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 750,
+      delay: function(el, i) {
+        return 50 * i;
+      }
+    }).add({
+      targets: '.ml6',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+
 });
