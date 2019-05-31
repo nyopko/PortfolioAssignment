e048eb1d45032082f2af46767df3bc9ae260ad60
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-05-31 13:36:31 -0400

    Fixed bug where navbar wouldnt close on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-$("#myButton").click(function() {
-    $("html, body").animate({
-        scrollTop: $("#aboutMe").offset().top
-    }, 2000);
-});
+    // auto scroll animation title screen
 
-$('.ml6 .letters').each(function(){
-    $(this).html($(this).text().replace(/[-A-Za-z0-9!$#%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/g, "<span class='letter'>$&</span>"));
-  });
-  
-  anime.timeline({loop: false})
-    .add({
-      targets: '.ml6  .letter',
-      translateY: ["1.1em", 0],
-      translateZ: 0,
-      duration: 750,
-      delay: function(el, i) {
-        return 50 * i;
-      }
+    $("#myButton").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#aboutMe").offset().top
+        }, 2000);
     });
+
+     // text animations title screen
+
+
+    $('.ml6 .letters').each(function () {
+        $(this).html($(this).text().replace(/[-A-Za-z0-9!$#%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/g, "<span class='letter'>$&</span>"));
+    });
+    
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.ml6  .letter',
+            translateY: ["1.1em", 0],
+            translateZ: 0,
+            duration: 750,
+            delay: function (el, i) {
+                return 50 * i;
+            }
+        });
+
+    // AOS scroll animations init
 
     AOS.init({
         duration: 1200,
-      })
+    });
+
+    // Making Nav Bar close on click for mobile
+
+    $('.navbar-collapse a').click(function (e) {
+        $('.navbar-collapse').collapse('toggle');
+    });
 
 });
