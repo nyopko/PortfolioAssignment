47d1a2d1b4662adf0a179ebbb4934b74661a3f5b
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-06-07 15:19:46 -0400

    changed target of button press on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
         $("#myButton").click(function () {
             $("html, body").animate({
-                scrollTop: $("#aboutRedirect").offset().top
+                scrollTop: $("#aboutMeIntro").offset().top
             }, 2000);
         });
     }
